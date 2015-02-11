1da52abe124a97ea5d2531817728c0498dc5017a
zl-leaf <cc.leaf92@gmail.com>
2015-02-11 13:10:58 +0800

    增加scheduler和downloader配置的检查和初始化

--- sosoinit/sosoinit.go
+++ sosoinit/sosoinit.go
@@ -1,8 +1,10 @@
-package main
+package sosoinit
 
 import(
 	"log"
 	"../configure"
+	"../scheduler"
+	"../downloader"
 	"../utils/db"
 )
 
@@ -10,18 +12,57 @@ const(
 	SCHEDULER = "scheduler"
 	DOWNLOADER = "downloader"
 	DATABASE = "database"
+
+	MASTER = "master"
+	PORT = "port"
+	DOWNLOAD_PATH = "download_path"
 )
 
-func main() {
+func Sosoinit() (scheduler *scheduler.Scheduler, downloaders []*downloader.Downloader){
 	config := configure.InitConfig("./config.ini")
+
+	if schedulerConfig,exist := config.GetEntity(SCHEDULER);exist {
+		checkSchedulerConfig(schedulerConfig)
+		scheduler = initScheduler(schedulerConfig)
+	}
+
+	if downloaderConfig,exist := config.GetEntity(DOWNLOADER);exist {
+		checkDownloaderConfig(downloaderConfig)
+		downloaders = initDownloaders(downloaderConfig)
+	}
+
 	if dbConfig,exist := config.GetEntity(DATABASE);exist {
 		checkDatabaseConfig(dbConfig)
 	} else {
 		log.Fatal("缺少数据库配置")
 	}
-	
+	return
 }
 
+/**
+ * 检查调度器的配置是否正确
+ */
+func checkSchedulerConfig(es []*configure.Entity) {
+	if len(es) > 1 {
+		//调度器只能有一个
+		log.Fatal("scheduler配置重复")
+	}
+}
+
+/**
+ * 检查下载器的配置是否正确
+ */
+func checkDownloaderConfig(es []*configure.Entity) {
+	for _,e := range es {
+		if e.GetAttr(MASTER) == "" {
+			log.Fatal("存在下载器没有对应master")
+		}
+	}
+}
+
+/**
+ * 检查数据库配置是否正确
+ */
 func checkDatabaseConfig(es []*configure.Entity) {
 	if len(es) > 1 {
 		log.Fatal("数据库配置重复")
@@ -44,4 +85,22 @@ func checkDatabaseConfig(es []*configure.Entity) {
 	if exist,_ := databaseConfig.CheckDBExist();!exist {
 		log.Fatal("数据库链接失败")
 	}
+}
+
+/**
+ * 初始化调度器
+ */
+func initScheduler(es []*configure.Entity) *scheduler.Scheduler{
+	e := es[0]
+	scheduler := scheduler.New(e.GetAttr(PORT))
+	return scheduler
+}
+
+func initDownloaders(es []*configure.Entity) []*downloader.Downloader {
+	downloaders := make([]*downloader.Downloader, 0)
+	for _,e := range es {
+		d := downloader.New(e.GetAttr(PORT), e.GetAttr(MASTER), e.GetAttr(DOWNLOAD_PATH))
+		downloaders = append(downloaders, d)
+	}
+	return downloaders
 }
