853f96defcc290cad41dd03218440c8d448d9c99
zl-leaf <cc.leaf92@gmail.com>
2015-03-01 17:01:58 +0800

    搜索范围结果带上weight参数

--- msg/searchmsg.go
+++ msg/searchmsg.go
@@ -3,7 +3,7 @@ package msg
 type SearchResultMsg struct {
 	Result int `json:"result"`
 	Msg string `json:"msg"`
-	Data []*SearchResultObj `json:"data"`
+	Data SearchDatas `json:"data"`
 }
 
 type SearchResultObj struct {
@@ -11,4 +11,16 @@ type SearchResultObj struct {
 	Title string `json:"title"`
 	Description string `json:"description"`
 	Keywords string `json:"keywords"`
+	Weight float64 `json:"weight"`
 }
+
+type SearchDatas []*SearchResultObj
+
+func (searchDatas SearchDatas) Len() int {return len(searchDatas)}
+func (searchDatas SearchDatas) Less(i,j int) bool {
+	if searchDatas[i].Weight > searchDatas[j].Weight {
+		return true
+	} 
+	return false
+} 
+func (searchDatas SearchDatas) Swap(i,j int) {searchDatas[i],searchDatas[j] = searchDatas[j],searchDatas[i]}
